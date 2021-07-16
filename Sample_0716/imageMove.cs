0bb0a524bdad8f69f41fdd91aad174b0edde4929
Jeong dalim <jeong.dalim@axrossroad.co.jp>
2021-07-16 14:26:08 +0900

    スクリプト作成方法

---
+++ Sample_0716/imageMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class imageMove : MonoBehaviour
+{
+    Vector3 StartPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+       Transform tr = this.GetComponent<Transform>();
+       this.StartPos = tr.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform tr = this.GetComponent<Transform>();
+        Vector3 pos = tr.position;
+        pos.x = this.StartPos.x + Random.Range(-2.0f, 2.0f);
+        pos.y = this.StartPos.y + Random.Range(-2.0f, 2.0f);
+        //pos.x = Random.Range(-2.0f, 2.0f);
+        //pos.y = Random.Range(-2.0f, 2.0f);
+        tr.position = pos;
+
+    }
+}
