e9361d0ff0afb5fda33f2d4550dd9891c40cce7b
Jeong dalim <jeong.dalim@axrossroad.co.jp>
2021-07-16 18:03:26 +0900

    commit

---
+++ The Box/Assets/Script/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public const int WALL_FRONT = 1;
+    public const int WALL_RIGHT = 2;
+    public const int WALL_BACK = 3;
+    public const int WALL_LEFT = 4;
+    public GameObject panelWalls;
+
+    private int wallNo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wallNo = WALL_FRONT;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
